3f736d97083321e627ecbcd40a0e62b4cefbc203
Abner Persio <binhopersio@gmail.com>
2025-01-12 12:01:55 -0300

    chore: file importing

--- src/infra/adapters/routes.ts
+++ src/infra/adapters/routes.ts
@@ -1,6 +1,6 @@
+import { errorHandler } from '@/infra/middlewares/error-handler';
 import middy from '@middy/core';
 import httpRouterHandler, { Route } from '@middy/http-router';
-import { errorHandler } from '../middlewares/error-handler';
 
 export function routesAdapt(routes: Route<any, any>[]) {
   return middy().use(errorHandler()).handler(httpRouterHandler(routes));
